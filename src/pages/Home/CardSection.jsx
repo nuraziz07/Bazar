bc4219e19702a9def759e9ee29205e3af45bbe12
NurazizCoder <nuralievnuraziz0@gmail.com>
2026-05-05 16:50:13 +0500

    m

--- src/pages/Home/CardSection.jsx
+++ src/pages/Home/CardSection.jsx
@@ -1,17 +1,42 @@
 import Card from '@/Components/Cards/card.jsx'
-import './loading.css'
+import notFound from '@/assets/png/404.png'
+import './loading.scss'
 import useAxios from '@/Hooks/useAxios.js'
 
 const CardSection = () => {
     const { data, loading, error } = useAxios('/products.json')
 
     if (error) {
-        return <h1>404</h1>
+        return (
+            <>
+                <img src={notFound} alt="" />
+            </>
+        )
     }
 
     return (
         <div className={'columns-2 gap-5'}>
-            {loading ? <h1>Loading...</h1> : <Card data={data} />}
+            {loading ? (
+                <svg
+                    className="spinner"
+                    width="65px"
+                    height="65px"
+                    viewBox="0 0 66 66"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle
+                        className="path"
+                        fill="none"
+                        stroke-width="6"
+                        stroke-linecap="round"
+                        cx="33"
+                        cy="33"
+                        r="30"
+                    ></circle>
+                </svg>
+            ) : (
+                <Card data={data} />
+            )}
         </div>
     )
 }
